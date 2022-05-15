718deb2b214113733de5a735ab6fb8ac43a127ea
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-05-16 00:29:19 +0200

    some changes

--- main/src/main/java/zw/co/rubiem/netone/portal/CommandLineAppStartupRunner.java
+++ main/src/main/java/zw/co/rubiem/netone/portal/CommandLineAppStartupRunner.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import zw.co.rubiem.netone.portal.usermanager.permissions.group.GroupPermissionService;
 import zw.co.rubiem.netone.portal.usermanager.permissions.permission.PermissionsService;
@@ -14,6 +15,7 @@ import zw.co.rubiem.netone.portal.usermanager.usergroup.UserGroupService;
 
 
 @Slf4j
+@Profile("winds")
 @Component
 @AllArgsConstructor
 public class CommandLineAppStartupRunner implements CommandLineRunner {
