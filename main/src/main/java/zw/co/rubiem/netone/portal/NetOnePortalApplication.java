9e612f3a3e76e2e7ad1a591e130817fff7e8659a
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-05-27 09:09:31 +0200

    refactoring

--- main/src/main/java/zw/co/rubiem/netone/portal/NetOnePortalApplication.java
+++ main/src/main/java/zw/co/rubiem/netone/portal/NetOnePortalApplication.java
@@ -3,7 +3,6 @@ package zw.co.rubiem.netone.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
